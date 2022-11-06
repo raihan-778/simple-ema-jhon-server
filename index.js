bc16de6a7caebaf8fab34a5fdaaff75b0b352673
unknown <raihanarif778@gmail.com>
2022-11-06 09:01:28 +0600

    api for pagination added

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require("express");
-const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express")
+const cors = require("cors")
+const { MongoClient, ServerApiVersion } = require("mongodb")
 
-require("dotenv").config();
-const app = express();
+require("dotenv").config()
+const app = express()
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
+
+//middleware
+app.use(cors())
+app.use(express.json())
 
 /*
  mongodb 
@@ -15,36 +19,33 @@ const port = process.env.PORT || 5000;
 
 //mongodb connection setup with database
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jz1qjld.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jz1qjld.mongodb.net/?retryWrites=true&w=majority`
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
-});
+})
 const run = async () => {
   try {
-    const productCollection = client.db("emaJhon").collection("products");
+    const productCollection = client.db("emaJhon").collection("products")
 
     app.get("/products", async (req, res) => {
-      const query = {};
-      const cursor = productCollection.find(query);
-      const products = await cursor.toArray();
-      res.send(products);
-    });
+      const query = {}
+      const cursor = productCollection.find(query)
+      const products = await cursor.toArray()
+      const count = await productCollection.estimatedDocumentCount()
+      res.send({ count, products })
+    })
   } finally {
   }
-};
-run().catch((err) => console.error(err));
-
-//middleware
-app.use(cors());
-app.use(express.json());
+}
+run().catch((err) => console.error(err))
 
 app.get("/", (req, res) => {
-  res.send("Simple Ema-john server is running");
-});
+  res.send("Simple Ema-john server is running")
+})
 
 app.listen(port, () => {
-  console.log(`Ema-jhon server is running on port ${port}`);
-});
+  console.log(`Ema-jhon server is running on port ${port}`)
+})
