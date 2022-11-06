91ae74e35df32e4b690922fcaac4b417e4df48c4
unknown <raihanarif778@gmail.com>
2022-11-06 09:39:36 +0600

    api modified

--- index.js
+++ index.js
@@ -31,7 +31,11 @@ const run = async () => {
     const productCollection = client.db("emaJhon").collection("products")
 
     app.get("/products", async (req, res) => {
+      const page = req.query.page
+      const size = req.query.size
+      console.log(page, size)
       const query = {}
+
       const cursor = productCollection.find(query)
       const products = await cursor.toArray()
       const count = await productCollection.estimatedDocumentCount()
